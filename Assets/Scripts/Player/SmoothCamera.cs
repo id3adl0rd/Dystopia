d313b59ca622f758ae3514a7e94d2ec7b06ba826
David <raindeadobzor2@gmail.com>
2024-01-19 13:48:51 +0300

    NPC (Выжидающий и с свободным поведением)

---
+++ Assets/Scripts/Player/SmoothCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    [SerializeField] private float _followSpeed;
+    [SerializeField] private Transform _target;
+
+    private void FixedUpdate()
+    {
+        Vector3 updatedPosition = new Vector3(_target.position.x, _target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, updatedPosition, _followSpeed * Time.deltaTime);
+    }
+}
